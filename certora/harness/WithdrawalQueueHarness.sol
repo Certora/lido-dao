01642066c7b273218df30288515de04f8d485adf
Gad Elbaz <gad@certora.com>
2022-12-28 18:54:32 +0200

    add getters

--- certora/harness/WithdrawalQueueHarness.sol
+++ certora/harness/WithdrawalQueueHarness.sol
@@ -5,4 +5,28 @@ import "../contracts/0.8.9/WithdrawalQueue.sol";
 
 contract WithdrawalQueueHarness is WithdrawalQueue {
 
+    function getRequestById(uint256 requestId) internal returns (Request) {
+
+    }
+
+    function getRequestsCumulativeEther(uint256 requestId) public returns (uint128) {
+        Request req = getRequestById(requestId);
+        return req.cumulativeEther;
+    }
+
+    function getRequestsCumulativeShares(uint256 requestId) public returns (uint128) {
+        Request req = getRequestById(requestId);
+        return req.cumulativeShares;
+    }
+
+    function getRequestsRecipient(uint256 requestId) public returns (address) {
+        Request req = getRequestById(requestId);
+        return req.recipient;
+    }
+
+    function isRequestClaimed(uint256 requestId) public returns (bool) {
+        Request req = getRequestById(requestId);
+        return req.claimed;
+    }
+
 }
--- certora/specs/WithdrawalQueue.spec
+++ certora/specs/WithdrawalQueue.spec
@@ -10,4 +10,22 @@ methods {
 
     //payable
     finalize(uint256, uint256, uint256, uint256)
+
+    // Getters:
+    finalizedRequestsCounter() returns (uint256) envfree
+    lockedEtherAmount() returns (uint128) envfree
 }
+
+rule integrityOfClaim() {
+
+}
+
+rule integrityOfEnqueue(address recipient, uint256 etherAmount, uint256 sharesAmount) {
+    env e;
+    
+    uint256 requestId = enqueue(e, recipient, etherAmount, sharesAmount);
+
+}
+
+// _sendValue function got native call - check for reentrency attack
+ 
