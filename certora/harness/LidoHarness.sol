4f5c199c2feae1a5672f499b754b5dca93bc867b
Gad Elbaz <gad@certora.com>
2023-04-23 14:44:33 +0300

    remove redundant

--- certora/harness/LidoHarness.sol
+++ certora/harness/LidoHarness.sol
@@ -22,11 +22,13 @@ contract LidoHarness is Lido {
     function collectRewardsAndProcessWithdrawals(
         uint256 _withdrawalsToWithdraw,
         uint256 _elRewardsToWithdraw,
-        uint256[] _withdrawalFinalizationBatches,
+        uint256 _withdrawalFinalizationBatch,
         uint256 _simulatedShareRate,
         uint256 _etherToLockOnWithdrawalQueue
     ) public {
         _requireReportValidTimeStamp();
+        uint256[] memory _withdrawalFinalizationBatches = new uint256[](1);
+        _withdrawalFinalizationBatches[0] = _withdrawalFinalizationBatch;
         _collectRewardsAndProcessWithdrawals(contracts, _withdrawalsToWithdraw, _elRewardsToWithdraw, _withdrawalFinalizationBatches, _simulatedShareRate, _etherToLockOnWithdrawalQueue);
     }
 
@@ -79,43 +81,6 @@ contract LidoHarness is Lido {
     function setOracleReportContracts() public {
         contracts = _loadOracleReportContracts();
     }
-
-    // function handleOracleReportWrapper(
-    //     // Oracle timings
-    //     uint256 _reportTimestamp,
-    //     uint256 _timeElapsed,
-    //     // CL values
-    //     uint256 _clValidators,
-    //     uint256 _clBalance,
-    //     // EL values
-    //     uint256 _withdrawalVaultBalance,
-    //     uint256 _elRewardsVaultBalance,
-    //     uint256 _sharesRequestedToBurn,
-    //     // Decision about withdrawals processing
-    //     uint256 _withdrawalFinalizationBatch,
-    //     uint256 _simulatedShareRate) public returns (uint256 a, uint256 b, uint256 c, uint256 d) {
-
-    //     uint256[] memory _withdrawalFinalizationBatches = new uint256[](1);
-    //     _withdrawalFinalizationBatches[0] = _withdrawalFinalizationBatch;
-
-    //     uint256[4] memory postRebaseAmounts; // = new uint256[](4);
-        
-    //     // postRebaseAmounts = handleOracleReport(_reportTimestamp, _timeElapsed, _clValidators, _clBalance, _withdrawalVaultBalance, _elRewardsVaultBalance, _sharesRequestedToBurn, _withdrawalFinalizationBatches, _simulatedShareRate);
-    //     postRebaseAmounts = _handleOracleReport(
-    //         OracleReportedData( 
-    //             _reportTimestamp,
-    //             _timeElapsed,
-    //             _clValidators,
-    //             _clBalance,
-    //             _withdrawalVaultBalance,
-    //             _elRewardsVaultBalance,
-    //             _sharesRequestedToBurn,
-    //             _withdrawalFinalizationBatches,
-    //             _simulatedShareRate
-    //         )
-    //     );
-    //     return (postRebaseAmounts[0], postRebaseAmounts[1], postRebaseAmounts[2], postRebaseAmounts[3]);
-    // }
 
     function stakingModuleMaxDepositsCount(uint256 _stakingModuleId, uint256 _maxValue) public view returns (uint256) {
         IStakingRouter stakingRouter = IStakingRouter(getLidoLocator().stakingRouter());
